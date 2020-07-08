d47e134314799072d2b4775711f151dce9d4c0ff
Renan Ostrowski <renancarlosmuraro@gmail.com>
2020-07-08 15:22:22 -0300

    Completed part 1

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
-  }
+    const income = this.transactions
+    .filter((income) => income.type === 'income')
+    .reduce((sum, transaction) => sum +  transaction.value, 0 );
 
-  public create(): Transaction {
-    // TODO
+    const outcome = this.transactions
+    .filter((outcome) => outcome.type === 'outcome')
+    .reduce((sum, transaction) => sum +  transaction.value, 0 );
+
+   const total = income - outcome;
+
+   const balance = {
+     income,
+     outcome,
+     total,
+   };
+
+   return balance;
+
+   }
+
+  public create({title, value, type} : CreateTransactionDTO): Transaction {
+    if(type === 'outcome'){
+      const newbalance = this.getBalance().total - value;
+      if(newbalance < 0){
+        throw new Error('Cannot complet transaction, insuficent balance');
+      }
+    }
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
